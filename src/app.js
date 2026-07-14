274b9831989eb5497ef2f66827f4d1787fb0e4a6
Jordy Munoz <jordy2004munoz@gmail.com>
2026-07-14 14:27:35 -0500

    Corregir middleware express json

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
-import express, { request } from 'express'
-//npm install cors
+import express from 'express';
 import cors from 'cors';
-import clientesRoutes from './routes/clientes.routes.js'
+
+import clientesRoutes from './routes/clientes.routes.js';
 import authRoutes from './routes/auth.routes.js';
 
+const app = express();
 
-
-
-const app= express();
-
-const corsOptions={
-    origin:'http://localhost:8100',
+const corsOptions = {
+    origin: [
+        'http://localhost:8100',
+        'http://localhost:8101'
+    ],
     methods:['GET','POST','PUT','PATCH','DELETE'],
     credentials:true
-}
+};
 
-app.use(cors(corsOptions));//habilitar los cors
-app.use(express.json());//para que interprete los objetos .json
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); // IMPORTANTE AQUÍ
+
+
 app.use('/api', authRoutes);
-//rutas 
-app.use('/api', clientesRoutes)
-app.use((req,res,next)=>{
-    res.status(400).json({
-        message: 'Endpoint not found'
-    })
-})
-export default app;
+app.use('/api', clientesRoutes);
 
-process.on('uncaughtException', (err) => {
-  console.error('Error no capturado:', err);
+
+app.use((req,res)=>{
+    res.status(400).json({
+        message:'Endpoint not found'
+    });
 });
+
+
+export default app;
